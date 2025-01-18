9aa2bb7ef827b67ea46a9f5eb7090aa4cad0b846
ZupaJul <44403676+juiian7@users.noreply.github.com>
2025-01-18 22:15:04 +0100

    Add engine concept

--- engine/Apate.ts
+++ engine/Apate.ts
@@ -1,9 +1,47 @@
-interface EngineConfig {}
+interface EngineConfig {
+    screen?: ScreenConfig;
+}
+
+interface ScreenConfig {
+    canvas?: HTMLCanvasElement;
+    // filter style
+    autoResize?: boolean;
+}
+
+const defaultConfig: EngineConfig = {};
+const defaultScreenConfig: ScreenConfig = {
+    autoResize: true,
+};
 
 export default class Apate {
-    constructor(config: EngineConfig) {}
+    constructor(config?: EngineConfig) {
+        // initialization
+        if (!config) config = defaultConfig;
 
-    async init() {}
+        // run after constructor
+        setTimeout(this._init.bind(this), 0);
+    }
 
-    update() {}
+    /** Internal init only (do not overwrite) */
+    private async _init() {
+        // - do engine init
+        // - do user init
+        await this.init();
+
+        // - start game loop
+        this._loop();
+    }
+
+    private _loop() {
+        // - do timings
+
+        this.update();
+
+        // - do timings
+        window.requestAnimationFrame(this._loop);
+    }
+
+    public async init() {}
+
+    public update() {}
 }
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,10 +1,21 @@
 import Apate from "../engine/index.js";
 
+const canvas = document.querySelector("canvas");
+
 class Game extends Apate {
+    counter: number = 0;
+
     async init(): Promise<void> {
-        return;
+        this.counter = 60;
     }
 
-    update(): void {}
+    update(): void {
+        this.counter--;
+
+        if (this.counter <= 0) {
+            console.log("Counter reached 0 -> setting to 60");
+            this.counter = 60;
+        }
+    }
 }
-new Game({});
+new Game({ screen: { canvas, autoResize: true } });
