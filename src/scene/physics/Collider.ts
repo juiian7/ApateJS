5f1a343d86934804a05e11c10ab5f72bb152b853
ZupaJul <44403676+juiian7@users.noreply.github.com>
2025-05-04 09:08:09 +0200

    Better way to add shapes to colliders

--- src/scene/physics/Collider.ts
+++ src/scene/physics/Collider.ts
@@ -2,9 +2,7 @@ import { Obj } from "../Obj.js";
 
 import { Context } from "../../graphics/Context.js";
 
-//import { Shape } from "./Shape.js";
 import { Apate } from "../../Apate.js";
-import { Vec } from "../../core/Vec.js";
 import { Shape } from "./shapes/Shape.js";
 import { CollisionInfo } from "../../core/Physics.js";
 
@@ -18,16 +16,17 @@ export class Collider<E extends Apate = Apate> extends Obj<E> {
     public collisionLayer: CollisionLayer = 0;
     public mask: number = 0xffff;
 
-    constructor(/* shapes: Shape[] = [], */ layer: CollisionLayer = 0, parent?: Obj, name?: string) {
+    constructor(shapes: Shape[] = [], layer: CollisionLayer = 0, parent?: Obj, name?: string) {
         super(parent, name);
 
-        //this.shapes = shapes;
+        this.shapes = [...shapes];
         this.collisionLayer = layer;
     }
 
     public addShape(shape: Shape) {
         this.shapes.push(shape);
         shape.transform.parent = this.transform;
+        return this;
     }
 
     public checkAgainst(col: Collider): CollisionInfo[] {
